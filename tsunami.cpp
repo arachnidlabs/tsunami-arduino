2fbb5df14763c480c4a8e316bfeb7bd6224620c9
Nick Johnson <arachnid@notdot.net>
2015-03-25 14:18:33 +0000

    Added support for timers

--- tsunami.cpp
+++ tsunami.cpp
@@ -37,10 +37,10 @@ void Tsunami_Class::begin() {
 	current_reg = 0;
 
 	// Initialize timer 1 to run at full speed and capture external events
-	TCCR1A &= ~_BV(WGM11) & ~_BV(WGM10);
-	TCCR1B &= ~_BV(WGM13) & ~_BV(WGM12) & ~_BV(CS12) & ~_BV(CS11);
-	TCCR1B |= _BV(CS10);
-	TIMSK1 |= _BV(ICIE1);
+	//TCCR1A &= ~_BV(WGM11) & ~_BV(WGM10);
+	//TCCR1B &= ~_BV(WGM13) & ~_BV(WGM12) & ~_BV(CS12) & ~_BV(CS11);
+	//TCCR1B |= _BV(CS10);
+	//TIMSK1 |= _BV(ICIE1);
 
 	dds.begin();
 }
