5b231a1ebd8bc3a3626d4268d787d5489ccf223c
Nick Johnson <arachnid@notdot.net>
2015-05-01 12:18:56 +1200

    Fix calls to spi_transfer for correct argument order

--- mcp49xx.c
+++ mcp49xx.c
@@ -6,13 +6,20 @@
 #define MCP49XX_DAC_SHDN 12
 #define MCP49XX_DAC_VALUE_MASK 0x0FFF
 
+static void _transfer(mcp49xx_t *dac, uint8_t idx) {
+	uint8_t data[2];
+	data[0] = dac->registers[idx] >> 8;
+	data[1] = dac->registers[idx] & 0xFF;
+	dac->spi_transfer(dac->ctx, data, sizeof(data));
+}
+
 void mcp49xx_init(mcp49xx_t *dac, spi_transfer_func func, void *ctx) {
 	dac->spi_transfer = func;
 	dac->ctx = ctx;
 	dac->registers[0] = 0;
 	dac->registers[1] = 1 << MCP49XX_DAC_ID;
-	dac->spi_transfer(dac->registers[0], sizeof(dac->registers[0]), dac->ctx);
-	dac->spi_transfer(dac->registers[1], sizeof(dac->registers[1]), dac->ctx);
+	_transfer(dac, 0);
+	_transfer(dac, 1);
 }
 
 void mcp49xx_set_is_buffered(mcp49xx_t *dac, uint8_t idx, uint8_t is_buffered) {
@@ -21,7 +28,7 @@ void mcp49xx_set_is_buffered(mcp49xx_t *dac, uint8_t idx, uint8_t is_buffered) {
 	} else {
 		dac->registers[idx] &= ~(1 << MCP49XX_DAC_BUF);
 	}
-	dac->spi_transfer(dac->registers[idx], sizeof(dac->registers[idx]), dac->ctx);
+	_transfer(dac, idx);
 }
 
 void mcp49xx_set_gain(mcp49xx_t *dac, uint8_t idx, mcp49xx_gain_t gain) {
@@ -30,7 +37,7 @@ void mcp49xx_set_gain(mcp49xx_t *dac, uint8_t idx, mcp49xx_gain_t gain) {
 	} else {
 		dac->registers[idx] |= 1 << MCP49XX_DAC_GAIN;
 	}
-	dac->spi_transfer(dac->registers[idx], sizeof(dac->registers[idx]), dac->ctx);
+	_transfer(dac, idx);
 }
 
 void mcp49xx_set_is_shutdown(mcp49xx_t *dac, uint8_t idx, uint8_t is_shutdown) {
@@ -39,11 +46,11 @@ void mcp49xx_set_is_shutdown(mcp49xx_t *dac, uint8_t idx, uint8_t is_shutdown) {
 	} else {
 		dac->registers[idx] |= 1 << MCP49XX_DAC_SHDN;
 	}
-	dac->spi_transfer(dac->registers[idx], sizeof(dac->registers[idx]), dac->ctx);
+	_transfer(dac, idx);
 }
 
 void mcp49xx_write(mcp49xx_t *dac, uint8_t idx, uint16_t value) {
 	dac->registers[idx] = (dac->registers[idx] & ~MCP49XX_DAC_VALUE_MASK)
 	  | (value & MCP49XX_DAC_VALUE_MASK);
-	dac->spi_transfer(dac->registers[idx], sizeof(dac->registers[idx]), dac->ctx);
+	_transfer(dac, idx);
 }
